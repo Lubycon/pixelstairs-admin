176831742f65c83a391334912d6f19b9af78647b
Evan Moon <bboydart91@gmail.com>
2017-07-15 15:43:50 +0900

    PXS-130: content image data has binded

--- src/app/components/modals/content-info-modal/content-info-modal.controller.js
+++ src/app/components/modals/content-info-modal/content-info-modal.controller.js
@@ -1,11 +1,16 @@
 export class ContentInfoModalController {
     constructor(
-        $scope, $mdDialog, contentId, APIService
+        $scope, $mdDialog, contentId,
+        APIService, ImageService
     ) {
         'ngInject';
 
         this.$mdDialog = $mdDialog;
         this.APIService = APIService;
+        this.ImageService = ImageService;
+
+        this.data = null;
+        this.contentImage = null;
 
         (this.init)(contentId);
     }
@@ -19,6 +24,7 @@ export class ContentInfoModalController {
             id
         }).get().then(res => {
             this.data = res.result;
+            this.contentImage = this.ImageService.setResolution(this.data.image, '640');
         }, err => {
             alert(`${id}번 컨텐츠를 불러오는 데 실패했습니다. 서버 관리자에게 문의하세요`);
         });
