e4da6314132bcd97e4565e22a7e4aa5fd828da22
Evan Moon <bboydart91@gmail.com>
2017-07-15 17:15:50 +0900

    PXS-130: user info modal has updated
    * user info modal style has updated
    * user detail api has bound to user info modal

--- src/app/components/modals/user-info-modal/user-info-modal.controller.js
+++ src/app/components/modals/user-info-modal/user-info-modal.controller.js
@@ -1,22 +1,31 @@
 export class UserInfoModalController {
     constructor(
-        $scope, $mdDialog, userId, APIService
+        $scope, $mdDialog, userId,
+        APIService, ImageService
     ) {
         'ngInject';
 
         this.$mdDialog = $mdDialog;
 
         this.APIService = APIService;
+        this.ImageService = ImageService;
 
         (this.init)(userId);
     }
 
     init(id) {
-        this.getUserData();
+        this.getUserData(id);
     }
 
-    getUserData() {
-        
+    getUserData(id) {
+        this.APIService.resource('members.detail', {
+            id
+        }).get().then(res => {
+            this.data = res.result;
+            this.userProfile = this.ImageService.getUserProfile(this.data.profileImg);
+        }, err => {
+            alert(`${id}번 유저 데이터를 불러오는 데 실패했습니다. 서버 관리자에게 문의하세요`);
+        });
     }
 
     close() {
