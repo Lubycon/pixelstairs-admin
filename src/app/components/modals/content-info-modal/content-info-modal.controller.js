2361f9355b6d079740587432f6ceced623cce269
Evan Moon <bboydart91@gmail.com>
2017-07-22 15:52:25 +0900

    PXS-130: Content delete modal has updated * Content delete modal has updated * angular-toasts has changed to $mdToast

--- src/app/components/modals/content-info-modal/content-info-modal.controller.js
+++ src/app/components/modals/content-info-modal/content-info-modal.controller.js
@@ -1,13 +1,13 @@
 export class ContentInfoModalController {
     constructor(
-        $scope, $mdDialog, toastr,
+        $scope, $mdDialog, $mdToast,
         contentId,
         APIService, ImageService
     ) {
         'ngInject';
 
         this.$mdDialog = $mdDialog;
-        this.toastr = toastr;
+        this.$mdToast = $mdToast;
         this.APIService = APIService;
         this.ImageService = ImageService;
 
@@ -51,10 +51,27 @@ export class ContentInfoModalController {
         this.APIService.resource('contents.detail', {
             id: data.id
         }).put(data).then(res => {
-            this.toastr.success(`${this.data.id}번 컨텐츠가 성공적으로 변경되었습니다`);
+            let toast = this.$mdToast.simple()
+                .position('top right')
+                .textContent(`${this.data.id}번 컨텐츠가 성공적으로 변경되었습니다`)
+                .hideDelay(3000);
+            this.$mdToast.show(toast);
+            // this.toastr.success(`${this.data.id}번 컨텐츠가 성공적으로 변경되었습니다`);
             this.isModifyEnable = false;
         }, err => {
-            this.toastr.error(`${this.data.id}번 컨텐츠변경에 실패하였습니다. 현상이 계속되면 서버관리자에게 문의하세요`);
+            // this.toastr.error(`${this.data.id}번 컨텐츠변경에 실패하였습니다. 현상이 계속되면 서버관리자에게 문의하세요`);
+        });
+    }
+
+    confirmDeleteContent() {
+        let confirm = this.$mdDialog.confirm()
+            .textContent(`${this.data.id}번 컨텐츠를 정말로 삭제하시겠어요?`)
+            .ok('네')
+            .cancel('다시 한번 생각해볼게요');
+        this.$mdDialog.show(confirm).then(res => {
+            console.log('delete');
+        }, err => {
+            console.log('no delete');
         });
     }
 }
