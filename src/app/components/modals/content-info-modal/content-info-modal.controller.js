729b3d753a8f5c3aa6e66a676d2186906913ad32
Evan Moon <bboydart91@gmail.com>
2017-07-22 15:33:55 +0900

    PXS-130: content modify function has updated

--- src/app/components/modals/content-info-modal/content-info-modal.controller.js
+++ src/app/components/modals/content-info-modal/content-info-modal.controller.js
@@ -1,16 +1,21 @@
 export class ContentInfoModalController {
     constructor(
-        $scope, $mdDialog, contentId,
+        $scope, $mdDialog, toastr,
+        contentId,
         APIService, ImageService
     ) {
         'ngInject';
 
         this.$mdDialog = $mdDialog;
+        this.toastr = toastr;
         this.APIService = APIService;
         this.ImageService = ImageService;
 
         this.data = null;
         this.contentImage = null;
+
+        this.isModifyEnable = false;
+        this.isContentDelete = false;
 
         (this.init)(contentId);
     }
@@ -34,18 +39,22 @@ export class ContentInfoModalController {
         this.$mdDialog.cancel();
     }
 
-    // TEST
-    testPut() {
-        let data = $.extend({}, this.data);
-        data.title = '어드민 업데이트 테스트 입니다. 1';
+    validateTag(tag) {
+        if(this.data.hashTags.indexOf(tag) > -1) {
+            console.log(this.data.hashTags);
+        }
+    }
+
+    putData() {
+        let data = angular.copy(this.data);
 
         this.APIService.resource('contents.detail', {
             id: data.id
         }).put(data).then(res => {
-            console.log(res);
+            this.toastr.success(`${this.data.id}번 컨텐츠가 성공적으로 변경되었습니다`);
+            this.isModifyEnable = false;
         }, err => {
-            console.log(err);
+            this.toastr.error(`${this.data.id}번 컨텐츠변경에 실패하였습니다. 현상이 계속되면 서버관리자에게 문의하세요`);
         });
     }
-    // TEST
 }
