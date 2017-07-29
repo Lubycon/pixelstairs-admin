3d8b8ba31d724508ab8b6840bdd117c81c7a526a
Evan Moon <bboydart91@gmail.com>
2017-07-29 16:33:39 +0900

    API_VERSION has added

--- src/app/config/restangular.config.js
+++ src/app/config/restangular.config.js
@@ -2,7 +2,7 @@
 
 export function restangularConfig(
     RestangularProvider,
-    API_HOST, IS_DEV, DEV_KEY, USER_AGENT, APP_VERSION, CUSTOM_HEADER_PREFIX
+    API_HOST, IS_DEV, DEV_KEY, USER_AGENT, API_VERSION, CUSTOM_HEADER_PREFIX
 ) {
     'ngInject';
 
@@ -10,7 +10,7 @@ export function restangularConfig(
     const $log = angular.injector(['ng']).get('$log');
 
     defaultHeaders['Content-Type'] = 'application/json';
-    defaultHeaders[CUSTOM_HEADER_PREFIX + 'version'] = APP_VERSION;
+    defaultHeaders[CUSTOM_HEADER_PREFIX + 'version'] = API_VERSION;
     defaultHeaders[CUSTOM_HEADER_PREFIX + 'device'] = 'bs=' + USER_AGENT.browser + ',dvc=' + USER_AGENT.device + ',os=' + USER_AGENT.os;
 
     defaultHeaders[CUSTOM_HEADER_PREFIX + 'country'] = 'KR';
